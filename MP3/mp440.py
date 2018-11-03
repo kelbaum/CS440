15e9a92cfdec1f918d9668f8ca33107b3fc9b50b
GHAngeloid <kelbaum1@gmail.com>
2018-11-03 04:17:17 -0400

    finished get_move_value and is_terminal_state

--- MP3/mp440.py
+++ MP3/mp440.py
@@ -1,32 +1,223 @@
 '''
 Compute the value brought by a given move by placing a new token for player
 at (row, column). The value is the number of opponent pieces getting flipped
-by the move. 
+by the move.
 
 A move is valid if for the player, the location specified by (row, column) is
 (1) empty and (2) will cause some pieces from the other player to flip. The
 return value for the function should be the number of pieces hat will be moved.
 If the move is not valid, then the value 0 (zero) should be returned. Note
-here that row and column both start with index 0. 
+here that row and column both start with index 0.
 '''
-#Kyle
 def get_move_value(state, player, row, column):
     flipped = 0
-    # Your implementation goes here 
+    # Your implementation goes here
+    if state[row][column] != ' ':
+        # Check if location is not empty
+        return 0
+    # Continue getting move value
+    other_player = ''
+    if player == 'B':
+        other_player = 'W'
+    else:
+        other_player = 'B'
+    flip_temp = 0
+
+    # check row ( - )
+    if row == 0:
+        for i in range(1, len(state)):
+            if state[i][column] == other_player:
+                flip_temp += 1
+            elif state[i][column] == player:
+                flipped += flip_temp
+                break
+            else:
+                break
+    elif 0 < row < len(state) - 1:
+        for i in range(row - 1, -1, -1):
+            if state[i][column] == other_player:
+                flip_temp += 1
+            elif state[i][column] == player:
+                flipped += flip_temp
+                break
+            else:
+                break
+        flip_temp = 0
+        for i in range(row + 1, len(state)):
+            if state[i][column] == other_player:
+                flip_temp += 1
+            elif state[i][column] == player:
+                flipped += flip_temp
+                break
+            else:
+                break
+    elif row + 1 == len(state):
+        for i in range(row - 1, -1, -1):
+            if state[i][column] == other_player:
+                flip_temp += 1
+            elif state[i][column] == player:
+                flipped += flip_temp
+                break
+            else:
+                break
+    flip_temp = 0
+
+    # check column ( | )
+    if column == 0:
+        for j in range(1, len(state[row])):
+            if state[row][j] == other_player:
+                flip_temp += 1
+            elif state[row][j] == player:
+                flipped += flip_temp
+                break
+            else:
+                break
+    elif 0 < column < len(state[row]) - 1:
+        for j in range(column - 1, -1, -1):
+            if state[row][j] == other_player:
+                flip_temp += 1
+            elif state[row][j] == player:
+                flipped += flip_temp
+                break
+            else:
+                break
+        flip_temp = 0
+        for j in range(column + 1, len(state)):
+            if state[row][j] == other_player:
+                flip_temp += 1
+            elif state[row][j] == player:
+                flipped += flip_temp
+                break
+            else:
+                break
+    elif column + 1 == len(state[row]):
+        for j in range(column - 1, -1, -1):
+            if state[row][j] == other_player:
+                flip_temp += 1
+            elif state[row][j] == player:
+                flipped += flip_temp
+                break
+            else:
+                break
+    flip_temp = 0
+
+    # check upper right diagonal ( / )
+    if row == 0 or column == len(state[row]) - 1:
+        y = column + 1
+        for i in range(row + 1, y):
+            if state[i][column - i] == other_player:
+                flip_temp += 1
+            elif state[i][column - i] == player:
+                flipped += flip_temp
+                break
+            else:
+                break
+    elif row == len(state) - 1 or column == 0:
+        x = row + 1
+        for j in range(column + 1, x):
+            if state[row - j][j] == other_player:
+                flip_temp += 1
+            elif state[row - j][j] == player:
+                flipped += flip_temp
+                break
+            else:
+                break
+    else:
+        i = row + 1
+        j = column - 1
+        while column != -1 or row != len(state):
+            if state[i][j] == other_player:
+                flip_temp += 1
+                i += 1
+                j -= 1
+            elif state[i][j] == player:
+                flipped += flip_temp
+                break
+            else:
+                break
+        flip_temp = 0
+        i = row - 1
+        j = column + 1
+        while row != -1 or column != len(state[row]):
+            if state[i][j] == other_player:
+                flip_temp += 1
+                i -= 1
+                j += 1
+            elif state[i][j] == player:
+                flipped += flip_temp
+                break
+            else:
+                break
+    flip_temp = 0
+
+    # check upper left diagonal ( \ )
+    if row == 0 or column == 0:
+        i = row + 1
+        j = column + 1
+        while i != len(state) and j != len(state[row]):
+            if state[i][j] == other_player:
+                flip_temp += 1
+                i += 1
+                j += 1
+            elif state[i][j] == player:
+                flipped += flip_temp
+                break
+            else:
+                break
+    elif row == len(state) - 1 or column == len(state[row]) - 1:
+        i = row - 1
+        j = column - 1
+        while i != -1 or j != -1:
+            if state[i][j] == other_player:
+                flip_temp += 1
+                i -= 1
+                j -= 1
+            elif state[i][j] == player:
+                flipped += flip_temp
+                break
+            else:
+                break
+    else:
+        i = row - 1
+        j = column - 1
+        while i != -1 and j != -1:
+            if state[i][j] == other_player:
+                flip_temp += 1
+                i -= 1
+                j -= 1
+            elif state[i][j] == player:
+                flipped += flip_temp
+                break
+            else:
+                break
+        flip_temp = 0
+        i = row + 1
+        j = column + 1
+        while row != len(state) or column != len(state[row]):
+            if state[i][j] == other_player:
+                flip_temp += 1
+                i += 1
+                j += 1
+            elif state[i][j] == player:
+                flipped += flip_temp
+                break
+            else:
+                break
+
     return flipped
 
 
 '''
 Execute a move that updates the state. A new state should be crated. The move
 must be valid. Note that the new state should be a clone of the old state and
-in particular, should not share memory with the old state. 
+in particular, should not share memory with the old state.
 '''
 def execute_move(state, player, row, column):
     #Make new state with applicable number of rows
     #Iterate by row across state and transition
 
     new_state = []
-    
+
     for x in range(0, len(state),1):
         new_state.append([])
         for y in range(0, len(state), 1):
@@ -34,7 +225,7 @@ def execute_move(state, player, row, column):
                 new_state[x].append(player)
             else:
                 new_state[x].append(state[x][y])
-    
+
     #Propagate changes caused by move
     #Up
     x = row - 1
@@ -148,7 +339,7 @@ def execute_move(state, player, row, column):
             for chX,chY in zip(range(row+1, x, 1), range(column+1, y, 1)):
                 new_state[chX][chY] = player
             break
-    
+
     return new_state
 
 '''
@@ -161,7 +352,7 @@ return value should be two tuple in the format of (blackpeices, white pieces), e
 def count_pieces(state):
     blackpieces = 0
     whitepieces = 0
-    
+
     for row in state:
         for item in row:
             if item == 'B':
@@ -171,46 +362,53 @@ def count_pieces(state):
     return (blackpieces, whitepieces)
 
 '''
-Check whether a state is a terminal state. 
+Check whether a state is a terminal state.
 '''
-#Kyle
 def is_terminal_state(state, state_list = None):
     terminal = False
-    # Your implementation goes here 
+    # Your implementation goes here
+    for i in len(state):
+        for j in len(state[i]):
+            if state[i][j] == ' ':
+                if get_move_value(state, 'B', i, j) != 0:
+                    return terminal
+                if get_move_value(state, 'W', i, j) != 0:
+                    return terminal
+    terminal = True
     return terminal
 
 '''
 The minimax algorithm. Your implementation should return the best value for the
-given state and player, as well as the next immediate move to take for the player. 
+given state and player, as well as the next immediate move to take for the player.
 '''
 def minimax(state, player):
     value = 0
     row = -1
     column = -1
-    # Your implementation goes here 
+    # Your implementation goes here
     return (value, row, column)
 
 '''
 This method should call the minimax algorithm to compute an optimal move sequence
-that leads to an end game. 
+that leads to an end game.
 '''
 def full_minimax(state, player):
     value = 0
     move_sequence = []
-    # Your implementation goes here 
+    # Your implementation goes here
     return (value, move_sequence)
 
 
 '''
 The minimax algorithm with alpha-beta pruning. Your implementation should return the
 best value for the given state and player, as well as the next immediate move to take
-for the player. 
+for the player.
 '''
 def minimax_ab(state, player, alpha = -10000000, beta = 10000000):
     value = 0
     row = -1
     column = -1
-    # Your implementation goes here 
+    # Your implementation goes here
     return (value, row, column)
 
 '''
@@ -220,7 +418,5 @@ that leads to an end game, using alpha-beta pruning.
 def full_minimax_ab(state, player):
     value = 0
     move_sequence = []
-    # Your implementation goes here 
+    # Your implementation goes here
     return (value, move_sequence)
-
-
